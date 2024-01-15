e04808c460c6af3d314f06381e9d8095e34771a4
Abdul <Shadowy.3bd@gmail.com>
2024-01-15 15:23:12 +0100

    "Bush"

--- src/main/java/be/intecbrussel/MathExpressionEvaluator/service/basicMathServiceImpl.java
+++ src/main/java/be/intecbrussel/MathExpressionEvaluator/service/basicMathServiceImpl.java
@@ -1,23 +1,56 @@
 package be.intecbrussel.MathExpressionEvaluator.service;
 
+import java.math.BigDecimal;
+
 public class basicMathServiceImpl implements BasicMathService {
     @Override
     public double add(double firstNumber, double secondNumber) {
-        return firstNumber+firstNumber ;
+
+        String firstNumberAsString = String.valueOf(firstNumber);
+        String secondNumberAsString = String.valueOf(secondNumber);
+
+        BigDecimal firstDecimal = new BigDecimal(firstNumberAsString);
+        BigDecimal secondDecimal = new BigDecimal(secondNumberAsString);
+
+        BigDecimal result = firstDecimal.add(secondDecimal);
+
+        System.out.println(firstDecimal);
+        System.out.println(secondDecimal);
+        System.out.println(result);
+
+
+        return result.doubleValue() ;
+        //return BigDecimal.valueOf(firstNumber).add(BigDecimal.valueOf(secondNumber).doubleValue());
     }
 
     @Override
-    public double subtract(double firstNumber, double secondNumber) {
-        return 0;
+    public double subtract(double firstNumber, double secondNumber)
+    {
+
+        String firstNumberAsString = String.valueOf(firstNumber);
+        String secondNumberAsString = String.valueOf(secondNumber);
+
+        BigDecimal firstDecimal = new BigDecimal(firstNumberAsString);
+        BigDecimal secondDecimal = new BigDecimal(secondNumberAsString);
+
+        BigDecimal result = firstDecimal.subtract(secondDecimal);
+
+        System.out.println(firstDecimal);
+        System.out.println(secondDecimal);
+        System.out.println(result);
+
+
+        return result.doubleValue() ;
+
     }
 
     @Override
     public double multiply(double firstNumber, double secondNumber) {
-        return 0;
+        return firstNumber*secondNumber;
     }
 
     @Override
     public double divide(double firstNumber, double secondNumber) {
-        return 0;
+        return firstNumber/secondNumber;
     }
 }
--- src/test/java/be/intecbrussel/MathExpressionEvaluator/BasicMathServiceTest.java
+++ src/test/java/be/intecbrussel/MathExpressionEvaluator/BasicMathServiceTest.java
@@ -3,12 +3,17 @@ package be.intecbrussel.MathExpressionEvaluator;
 import be.intecbrussel.MathExpressionEvaluator.service.BasicMathService;
 import be.intecbrussel.MathExpressionEvaluator.service.basicMathServiceImpl;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 
-public class BasicMathServiceTest {
-    private BasicMathService basicMathService ;
+    public class BasicMathServiceTest {
+    private BasicMathService basicMathService;
 
     @BeforeAll
     public void beforeAll() {
@@ -17,19 +22,127 @@ public class BasicMathServiceTest {
     }
 
     @Test
+    public void testBasicAdditionalOfTowIntegers() {
 
-    public void testBasicAdditionalOfTowIntegers(){
+        int firstNumber = 7;
+        int secondNumber = 6;
 
-        int firstNumber = 7 ;
-        int secondNumber = 6 ;
+        int expectedValue = 13;
 
-        int expectedValue = 14 ;
+        double result = basicMathService.add(firstNumber, secondNumber);
 
-        double result = basicMathService.add(firstNumber,secondNumber);
+        assertEquals(expectedValue, result);
+    }
 
-        Assertions.assertEquals(expectedValue , result);
+    @Test
+    public void testBasicAdditionOfTwoNegativeNumbers() {
+        int firstNumber = -7;
+        int secondNumber = -6;
+
+        int expectedResult = -13;
+
+        double result = basicMathService.add(firstNumber, secondNumber);
+
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    // @ValueSource
+    @ParameterizedTest
+    @MethodSource("basicAdditionFactory")
+    public void testBasicAdditions(double firstNumber, double secondNumber, double expectedResult) {
+        double result = basicMathService.add(firstNumber, secondNumber);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    public static Stream<Arguments> basicAdditionFactory() {
+        return Stream.of(
+                Arguments.of(5, 3, 8),
+                Arguments.of(50, 3, 53),
+                Arguments.of(-5, 3, -2),
+                Arguments.of(0, 0, 0),
+                Arguments.of(-7, -3, -10),
+                Arguments.of(5, -3, 2),
+                Arguments.of(5.5, 4.5, 10),
+                Arguments.of(2000000000, 2000000000, 4000000000L),
+                Arguments.of(-0.00001, 0.00002, 0.00001),
+                //Arguments.of(-0.000_01 , 0.000_02 , 0.000_01),
+                Arguments.of(5.5, 4.5, 10)
+        );
 
     }
 
+    @ParameterizedTest
+    @MethodSource("basicSubFactory")
+    public void testBasicOfTowNumbersSubtract(double firstNumber, double secondNumber, double expectedResult) {
 
+
+        double result = basicMathService.subtract(firstNumber, secondNumber);
+        Assertions.assertEquals(expectedResult, result);
+
+
+    }
+
+    public static Stream<Arguments> basicSubFactory() {
+        return Stream.of(
+                Arguments.of(5, 3, 2),
+                Arguments.of(50, 3, 47),
+                Arguments.of(-5, 3, -8),
+                Arguments.of(0, 0, 0),
+                Arguments.of(-7, -3, -4),
+                Arguments.of(5, -3, 8),
+                Arguments.of(4.5, 5.4, -0.9),
+                //Arguments.of(2000000000 , 4000000000L , - 2000000000),
+                Arguments.of(-0.00001, -0.00002, 1E-5),
+                //Arguments.of(-0.00001 , 0.00002 , 0.000_01),
+                Arguments.of(5.5, 4.5, 1)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("basicMultiFactory")
+    public void testBasicOfTowNumbersMulti(double firstNumber, double secondNumber, double expectedResult)
+    {
+        double result = basicMathService.multiply(firstNumber, secondNumber);
+        Assertions.assertEquals(expectedResult, result);
+    }
+    public static Stream<Arguments> basicMultiFactory() {
+        return Stream.of(
+                Arguments.of(5, 3, 15.0),
+                Arguments.of(50, 3, 150),
+                Arguments.of(-5, 3, -15),
+                Arguments.of(0, 0, 0),
+                Arguments.of(-7, -3, 21),
+                Arguments.of(5, -3, -15),
+                Arguments.of(4.5, 5.4,24.3 ),
+                //Arguments.of(2000000000 , 4000000000L , - 2000000000),
+                Arguments.of(-0.00001, -0.00002, 2.0000000000000003E-10),
+                //Arguments.of(-0.000_01 , 0.000_02 , 0.000_01),
+                Arguments.of(5.5, 4.5, 24.75)
+        );
+
+    }
+    @ParameterizedTest
+    @MethodSource("basicDividFactory")
+    public void testBasicOfTowNumbersDivide(double firstNumber, double secondNumber, double expectedResult)
+    {
+        double result = basicMathService.divide(firstNumber, secondNumber);
+        Assertions.assertEquals(expectedResult, result);
+    }
+    public static Stream<Arguments> basicDividFactory() {
+        return Stream.of(
+                Arguments.of(5, 3, 1.6666666666666667),
+                Arguments.of(50, 3, 16.666666666666668),
+                Arguments.of(-5, 3, -1.6666666666666667),
+         //       Arguments.of(0, 0, 0),
+                Arguments.of(-7, -3, 2.3333333333333335),
+                Arguments.of(5, -3, -1.6666666666666667),
+                Arguments.of(4.5, 5.4, 0.8333333333333333),
+                //Arguments.of(2000000000 , 4000000000L , - 2000000000),
+                Arguments.of(-0.00001, -0.00002, 0.5),
+                //Arguments.of(-0.000_01 , 0.000_02 , 0.000_01),
+                Arguments.of(5.5, 4.5, 1.2222222222222223)
+        );
+
+
+    }
 }
+
